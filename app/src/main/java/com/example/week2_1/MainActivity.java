91a0f902cbf3b48c33f3cf833ba564accd2d0036
HieuNguyen <hieu1282003@gmail.com>
2023-09-13 10:14:09 +0700

    done

---
+++ app/src/main/java/com/example/week2_1/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.week2_1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.NumberPicker;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b1,b2,b3,b4;
+    NumberPicker numberPicker;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.mymenu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.relative) {
+            setContentView(R.layout.relative);
+            numberPicker = findViewById(R.id.numpick);
+            numberPicker.setMinValue(0);
+            numberPicker.setMaxValue(1000);
+        } else if (id == R.id.linear) {
+            setContentView(R.layout.linear);
+            numberPicker = findViewById(R.id.numpick);
+            numberPicker.setMinValue(0);
+            numberPicker.setMaxValue(1000);
+        } else if (id == R.id.table) {
+            setContentView(R.layout.table);
+            numberPicker = findViewById(R.id.numpick);
+            numberPicker.setMinValue(0);
+            numberPicker.setMaxValue(1000);
+        } else if (id == R.id.constraint) {
+            setContentView(R.layout.constraint);
+            numberPicker = findViewById(R.id.numpick);
+            numberPicker.setMinValue(0);
+            numberPicker.setMaxValue(1000);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
