ef4b466f60866c114c5d1aa16c7ba663aeff2ee3
HieuNguyen <hieu1282003@gmail.com>
2023-10-08 12:55:28 +0700

    done

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Content Provider 2"
+include(":app")
+ 
